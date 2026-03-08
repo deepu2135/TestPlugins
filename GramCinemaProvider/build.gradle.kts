3e8a403527f55c6951e9eac8297d32b39b2cc4c9
deepu2135 <deepakmahato8271@gmail.com>
2026-03-08 22:45:02 +0530

    Update build.gradle.kts

--- GramCinemaProvider/build.gradle.kts
+++ GramCinemaProvider/build.gradle.kts
@@ -1,1 +1,8 @@
-h
+version = 1
+
+cloudstream {
+    description = "Bollywood, Hollywood, Tamil & Telugu movies and TV shows from Gram Cinema"
+    authors = listOf("deepu2135")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "hi"
