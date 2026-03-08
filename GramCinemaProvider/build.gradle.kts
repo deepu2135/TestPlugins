58e06d0ee90a8ca61122446c4857beb4f3b6fbad
deepu2135 <deepakmahato8271@gmail.com>
2026-03-08 22:55:13 +0530

    Remove tvTypes from build.gradle.kts

--- GramCinemaProvider/build.gradle.kts
+++ GramCinemaProvider/build.gradle.kts
@@ -4,5 +4,4 @@ cloudstream {
     description = "Bollywood, Hollywood, Tamil & Telugu movies and TV shows from Gram Cinema"
     authors = listOf("deepu2135")
     status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-    
+}
