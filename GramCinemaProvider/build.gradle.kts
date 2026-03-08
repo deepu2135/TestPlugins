e793842fd987d59e2721b7b13ac9665fd789d6d0
deepu2135 <deepakmahato8271@gmail.com>
2026-03-08 22:59:33 +0530

    Update build.gradle.kts

--- GramCinemaProvider/build.gradle.kts
+++ GramCinemaProvider/build.gradle.kts
@@ -1,7 +1,13 @@
 version = 1
 
 cloudstream {
-    description = "Bollywood, Hollywood, Tamil & Telugu movies and TV shows from Gram Cinema"
+    description = "Bollywood, Hollywood, Tamil & Telugu movies from Gram Cinema"
     authors = listOf("deepu2135")
     status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    language = "hi"
+    iconUrl = "https://bollywood.eu.org/favicon.ico"
 }
