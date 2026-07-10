2298c88e69538fa6af11f84dc8bcb8d1568c69d5
deepu2135 <deepakmahato8271@gmail.com>
2026-07-10 22:46:32 +0530

    Force cancel active downloads when jumping offsets to prevent TDLib from getting stuck sequentially downloading large MKV files

--- TelegramProvider/build.gradle.kts
+++ TelegramProvider/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 }
 
 // Use an integer for version numbers
-version = 67
+version = 68
 
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
