3b1b77a5201ce21589dba8fef93b33265afda44c
deepu2135 <deepakmahato8271@gmail.com>
2026-07-11 11:37:40 +0530

    Bump version to 72

--- TelegramProvider/build.gradle.kts
+++ TelegramProvider/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 }
 
 // Use an integer for version numbers
-version = 71
+version = 72
 
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
