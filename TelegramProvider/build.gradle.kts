e53bb6153e3dcb6236284b4ffa4a07b1188f78ce
deepu2135 <deepu2135@users.noreply.github.com>
2026-07-17 19:02:22 +0530

    Fix topic items showing as movie by adding TvSeries to tvTypes & bump version to 86

--- TelegramProvider/build.gradle.kts
+++ TelegramProvider/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 }
 
 // Use an integer for version numbers
-version = 85
+version = 86
 
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
@@ -26,7 +26,7 @@ cloudstream {
     * **/
     status = 1 // Will be 3 if unspecified
 
-    tvTypes = listOf("Movie")
+    tvTypes = listOf("Movie", "TvSeries")
 
     requiresResources = false // Set to false since we build UI dynamically in Kotlin
     language = "en"
