6adea93dbc3770794bcd21d4fa7bc81650a5d045
deepu2135 <deepu2135@users.noreply.github.com>
2026-07-17 17:44:02 +0530

    Bump version to 81

--- TelegramProvider/build.gradle.kts
+++ TelegramProvider/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 }
 
 // Use an integer for version numbers
-version = 80
+version = 81
 
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
